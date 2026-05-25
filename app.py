a64cdac47e9b7d24303c7b57f1cf4b26245a4103
Henrique06Paiva <henriqueesteves06@gmail.com>
2026-05-25 13:53:20 -0300

    lock: mover credentials para os secrets e atualizar gitignore

--- app.py
+++ app.py
@@ -27,12 +27,22 @@ def autenticar_agenda():
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            # LÊ DIRETO DOS SECRETS DO STREAMLIT
+            if "GOOGLE_CREDENTIALS" in st.secrets:
+                # Transforma o bloco TOML em um dicionário Python que o Google reconhece
+                creds_dict = {"installed": dict(st.secrets["GOOGLE_CREDENTIALS"]["installed"])}
+                flow = InstalledAppFlow.from_client_config(creds_dict, SCOPES)
+                creds = flow.run_local_server(port=0)
+            else:
+                # Fallback para rodar local caso o arquivo ainda exista na sua máquina
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+                
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
